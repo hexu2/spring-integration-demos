33ba18a2a04613c1385eb6e303fac07b0a062afa
hexu <2212925883@qq.com>
2020-04-28 14:29:01 +0800

    create si-cafe demo modules

---
+++ si-cafe/src/test/java/com/hexu/si/cafe/sicafe/SiCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexu.si.cafe.sicafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
