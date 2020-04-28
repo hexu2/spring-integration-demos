fa80c25f0e91e99739e8b4a69a0743f37002fdcb
hexu <2212925883@qq.com>
2020-04-28 16:48:14 +0800

    create si-cafe demo modules

--- si-cafe/src/main/java/com/hexu/si/cafe/entity/OderItem.java
+++ si-cafe/src/main/java/com/hexu/si/cafe/entity/OderItem.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class OderItem implements Serializable {
 
+    private int shots = 1;
+
     private int orderNumber;
 
     private DrinkType drinkType;
@@ -11,8 +13,55 @@ public class OderItem implements Serializable {
     private boolean iced;
 
 
+    public OderItem() {
+    }
 
+    public OderItem(int shots, int orderNumber, DrinkType drinkType, boolean iced) {
+        this.shots = shots;
+        this.orderNumber = orderNumber;
+        this.drinkType = drinkType;
+        this.iced = iced;
+    }
 
+    public int getShots() {
+        return shots;
+    }
 
+    public void setShots(int shots) {
+        this.shots = shots;
+    }
 
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public DrinkType getDrinkType() {
+        return drinkType;
+    }
+
+    public void setDrinkType(DrinkType drinkType) {
+        this.drinkType = drinkType;
+    }
+
+    public boolean isIced() {
+        return iced;
+    }
+
+    public void setIced(boolean iced) {
+        this.iced = iced;
+    }
+
+    @Override
+    public String toString() {
+        return "OderItem{" +
+                "shots=" + shots +
+                ", orderNumber=" + orderNumber +
+                ", drinkType=" + drinkType +
+                ", iced=" + iced +
+                '}';
+    }
 }
