0cfa78da425ec9dca912b1438c99d55e6910dfc8
hexu <2212925883@qq.com>
2020-04-28 12:34:14 +0800

    create si-message demo modules

---
+++ si-message/src/test/java/com/hexu/si/message/simessage/SiMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexu.si.message.simessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
